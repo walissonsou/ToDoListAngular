3e007beac88d9295f9afc4f2f67e73973aa9c2c2
walissonsou <walissonsouza96@hotmail.com>
2022-08-22 23:55:25 -0300

    Todo css finalizado

--- src/app/module/home/components/to-do/to-do.component.ts
+++ src/app/module/home/components/to-do/to-do.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
-  styleUrls: ['./to-do.component.css']
+  styleUrls: ['./to-do.component.scss']
 })
 export class ToDoComponent implements OnInit {
+
+  public taskList: Array<{task: string, checked: boolean}> = []
+
 
   constructor() { }
 
