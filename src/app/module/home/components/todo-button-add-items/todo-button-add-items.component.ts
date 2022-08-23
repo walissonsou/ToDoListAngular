d948890ab9816f965a34cc5d4dc0f5a29aa88dbb
walissonsou <walissonsouza96@hotmail.com>
2022-08-22 23:55:38 -0300

    sCss Finalizado

--- src/app/module/home/components/todo-button-add-items/todo-button-add-items.component.ts
+++ src/app/module/home/components/todo-button-add-items/todo-button-add-items.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-button-add-items',
   templateUrl: './todo-button-add-items.component.html',
-  styleUrls: ['./todo-button-add-items.component.css']
+  styleUrls: ['./todo-button-add-items.component.scss']
 })
 export class TodoButtonAddItemsComponent implements OnInit {
 
